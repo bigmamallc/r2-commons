b0602f04787a4fe542ec3425bf946a7b53aa7ebc
ivan <im@bigma.org>
2021-10-13 17:13:00 -0400

    change log level for http log mw based on conditions

--- api/http/logmw.go
+++ api/http/logmw.go
@@ -3,6 +3,7 @@ package http
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"net/http"
 	"regexp"
 	"time"
 )
@@ -18,7 +19,13 @@ func RequestLogMW(log zerolog.Logger, skipRegex *regexp.Regexp) gin.HandlerFunc 
 		ctx.Next()
 		dur := time.Now().Sub(start)
 
-		log.Info().
+		ev := log.Debug()
+		if ctx.Writer.Status() >= 500 {
+			ev = log.Warn()
+		} else if ctx.Request.Method != http.MethodGet || (ctx.Writer.Status() < 200 || ctx.Writer.Status() >= 300) {
+			ev = log.Info()
+		}
+		ev.
 			Str("method", ctx.Request.Method).
 			Str("uri", ctx.Request.RequestURI).
 			Int("status", ctx.Writer.Status()).
