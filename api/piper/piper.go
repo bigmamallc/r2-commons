c724a54e29c80d0ad3d830ae52d6f580f4cb6653
ivan <im@bigma.org>
2021-10-28 16:40:33 -0400

    add missing metrics to piper

--- api/piper/piper.go
+++ api/piper/piper.go
@@ -15,9 +15,10 @@ type Piper struct {
 	bufferSize   int
 	writeTimeout time.Duration
 
-	totalBytesIn  prometheus.Counter
-	totalBytesOut prometheus.Counter
-	ioErrors      prometheus.Counter
+	activeUniPipes prometheus.Gauge
+	totalBytesAtoB prometheus.Counter
+	totalBytesBtoA prometheus.Counter
+	ioErrors       prometheus.Counter
 }
 
 func New(log zerolog.Logger, mxReg *prometheus.Registry, mxSubsystem string,
@@ -28,11 +29,12 @@ func New(log zerolog.Logger, mxReg *prometheus.Registry, mxSubsystem string,
 		bufferSize:   bufferSize,
 		writeTimeout: writeTimeout,
 
-		totalBytesIn:  prometheus.NewCounter(prometheus.CounterOpts{Name: "totalBytesIn", Subsystem: mxSubsystem}),
-		totalBytesOut: prometheus.NewCounter(prometheus.CounterOpts{Name: "totalBytesOut", Subsystem: mxSubsystem}),
-		ioErrors:      prometheus.NewCounter(prometheus.CounterOpts{Name: "ioErrors", Subsystem: mxSubsystem}),
+		activeUniPipes: prometheus.NewGauge(prometheus.GaugeOpts{Name: "activeUniPipes", Subsystem: mxSubsystem}),
+		totalBytesAtoB: prometheus.NewCounter(prometheus.CounterOpts{Name: "totalBytesAtoB", Subsystem: mxSubsystem}),
+		totalBytesBtoA: prometheus.NewCounter(prometheus.CounterOpts{Name: "totalBytesBtoA", Subsystem: mxSubsystem}),
+		ioErrors:       prometheus.NewCounter(prometheus.CounterOpts{Name: "ioErrors", Subsystem: mxSubsystem}),
 	}
-	mxReg.MustRegister(p.totalBytesIn, p.totalBytesOut, p.ioErrors)
+	mxReg.MustRegister(p.activeUniPipes, p.totalBytesAtoB, p.totalBytesBtoA, p.ioErrors)
 	return p
 }
 
@@ -44,13 +46,18 @@ func (p *Piper) StartBidiPipe(connA net.Conn, connB net.Conn) {
 		baLog = baLog.With().Str("dir", "b->a").Logger()
 	}
 
-	go p.uniPipe(connA, connB, abLog)
-	go p.uniPipe(connB, connA, baLog)
+	go p.uniPipe(connA, connB, abLog, p.totalBytesAtoB)
+	go p.uniPipe(connB, connA, baLog, p.totalBytesBtoA)
 }
 
-func (p *Piper) uniPipe(from net.Conn, to net.Conn, log zerolog.Logger) {
+func (p *Piper) uniPipe(from net.Conn, to net.Conn, log zerolog.Logger, byteMetric prometheus.Counter) {
 	log.Debug().Msg("entering uniPipe()")
+
+	p.activeUniPipes.Inc()
+
 	defer func() {
+		p.activeUniPipes.Dec()
+
 		from.Close()
 		to.Close()
 		log.Debug().Msg("exiting uniPipe()")
@@ -75,6 +82,8 @@ func (p *Piper) uniPipe(from net.Conn, to net.Conn, log zerolog.Logger) {
 				log.Debug().Err(writeErr).Msg("to.Write() failed")
 				return
 			}
+
+			byteMetric.Add(float64(writeSize))
 		}
 		if errors.Is(readErr, io.EOF) {
 			log.Debug().Msg("io.EOF from from.Read() - closing from and exiting")
