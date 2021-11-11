067d6ec2b6ed0adfd544b41d10704c25b6bc61a5
ivan <im@bigma.org>
2021-11-10 21:07:53 -0500

    redis health

---
+++ api/health/redis.go
@@ -0,0 +1,23 @@
+package health
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+)
+
+type RedisComponent struct {
+	name string
+	r    *redis.Client
+}
+
+func (r *RedisComponent) HealthComponentName() string {
+	return r.name
+}
+
+func (r *RedisComponent) CheckHealthy() (bool, string) {
+	_, err := r.r.Ping(context.Background()).Result()
+	if err != nil {
+		return false, err.Error()
+	}
+	return true, "ok"
+}
